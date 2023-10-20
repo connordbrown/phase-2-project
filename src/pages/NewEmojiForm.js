e4306a5c0336091230d2c208a15dfd8195a86fd8
Connor <connordbrown@gmail.com>
2023-10-20 16:22:37 -0600

    add CSS for EmojiForm

--- src/pages/NewEmojiForm.js
+++ src/pages/NewEmojiForm.js
@@ -1,7 +1,48 @@
 import React from 'react';
+import '../components/App.css';
 
 function NewEmojiForm() {
-    return <h1>New emoji creation form here!</h1>
+    return (
+        <div className="container">
+          <form className="add-emoji-form">
+            <h3>Add a new emoji!</h3>
+            <input
+              type="text"
+              name="name"
+              placeholder="name"
+              className="input-text"
+            />
+            <br />
+            <input
+              type="text"
+              name="image"
+              placeholder="category"
+              className="input-text"
+            />
+            <br />
+            <input
+              type="text"
+              name="image"
+              placeholder="group"
+              className="input-text"
+            />
+            <br />
+            <input
+              type="text"
+              name="image"
+              placeholder="html code"
+              className="input-text"
+            />
+            <br />
+            <input
+              type="submit"
+              name="submit"
+              value="Create New Emoji"
+              className="submit"
+            />
+          </form>
+        </div>
+      )
 }
 
 export default NewEmojiForm;
