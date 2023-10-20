41fef54912acc61d840ac7e969fca6274e36bc0f
Connor <connordbrown@gmail.com>
2023-10-19 20:21:44 -0600

    add EmojiInfo page with dynamic endpoint

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import NavBar from './NavBar';
 import Home from '../pages/Home';
 import About from '../pages/About';
 import NewEmojiForm from '../pages/NewEmojiForm';
+import EmojiInfo from '../pages/EmojiInfo';
 import ErrorPage from '../pages/ErrorPage.js';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/new-emoji-form" element={<NewEmojiForm />} />
+        <Route path="/emoji/:name" element={<EmojiInfo />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
