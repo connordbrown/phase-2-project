4dad166f9b25ee7095e6566f0fa7d58a5c83bba9
Connor <connordbrown@gmail.com>
2023-10-20 16:40:31 -0600

    add EmojiForm component functionality

---
+++ src/components/EmojiForm.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import '../components/App.css';
+
+function EmojiForm({ onNewEmojiSubmit }) {
+    const [name, setName] = useState("");
+    const [category, setCategory] = useState("");
+    const [group, setGroup] = useState("");
+    const [htmlCode, setHtmlCode] = useState("");
+
+    return (
+        <div className="container">
+          <form className="add-emoji-form">
+            <h3><em>Add a new emoji!</em></h3>
+            <input
+              type="text"
+              name="name"
+              placeholder="name"
+              className="input-text"
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
+            <br />
+            <input
+              type="text"
+              name="category"
+              placeholder="category"
+              className="input-text"
+              value={category}
+              onChange={e => setCategory(e.target.value)}
+            />
+            <br />
+            <input
+              type="text"
+              name="group"
+              placeholder="group"
+              className="input-text"
+              value={group}
+              onChange={e => setGroup(e.target.value)}
+            />
+            <br />
+            <input
+              type="text"
+              name="html-code"
+              placeholder="html code"
+              className="input-text"
+              value={htmlCode}
+              onChange={e => setHtmlCode(e.target.value)}
+            />
+            <br />
+            <input
+              type="submit"
+              name="submit"
+              value="Create New Emoji"
+              className="submit"
+              onSubmit={onNewEmojiSubmit}
+            />
+          </form>
+        </div>
+      )
+}
+
+export default EmojiForm;
--- src/pages/NewEmojiForm.js
+++ src/pages/NewEmojiForm.js
@@ -1,48 +1,8 @@
 import React from 'react';
-import '../components/App.css';
+import EmojiForm from '../components/EmojiForm'
 
 function NewEmojiForm() {
-    return (
-        <div className="container">
-          <form className="add-emoji-form">
-            <h3><em>Add a new emoji!</em></h3>
-            <input
-              type="text"
-              name="name"
-              placeholder="name"
-              className="input-text"
-            />
-            <br />
-            <input
-              type="text"
-              name="image"
-              placeholder="category"
-              className="input-text"
-            />
-            <br />
-            <input
-              type="text"
-              name="image"
-              placeholder="group"
-              className="input-text"
-            />
-            <br />
-            <input
-              type="text"
-              name="image"
-              placeholder="html code"
-              className="input-text"
-            />
-            <br />
-            <input
-              type="submit"
-              name="submit"
-              value="Create New Emoji"
-              className="submit"
-            />
-          </form>
-        </div>
-      )
+  return <EmojiForm />  
 }
 
 export default NewEmojiForm;
