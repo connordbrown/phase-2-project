38ddd12279dd450ab51805f071ae09a0cb1ae024
Connor <connordbrown@gmail.com>
2023-10-21 16:02:59 -0600

    comment App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// styling for App and child components
 import './App.css';
 import NavBar from './NavBar';
+// pages for routing
 import Home from '../pages/Home';
 import About from '../pages/About';
 import NewEmojiForm from '../pages/NewEmojiForm';
 import EmojiInfo from '../pages/EmojiInfo';
 import ErrorPage from '../pages/ErrorPage.js';
 
+// parent function of application
 function App() {
+  // create state for main emoji data
   const [emojiList, setEmojiList] = useState([]);
+  // create state for asynchronous page rendering
   const [isLoaded, setIsLoaded] = useState(false);
 
+  // fetch emoji data from server, then set states
   useEffect(() => {
     fetch("http://localhost:3000/emojis")
     .then(response => response.json())
@@ -22,6 +28,8 @@ function App() {
     .catch(error => alert(error.message))
   }, [])
 
+  // post a new emoji to the backend and render it on the page
+  // params: event (object), emojiName (string), emojiCategory (string), emojiGroup(string), emojiHtmlCode(string)
   function handleAddNewEmoji(event, emojiName, emojiCategory, emojiGroup, emojiHtmlCode) {
     event.preventDefault();
     fetch("http://localhost:3000/emojis", {
@@ -40,16 +48,19 @@ function App() {
     })
     .then(response => response.json())
     .then(newEmoji => {
+      // add newEmoji to emojiList and update state to show change on page
       const updatedEmojis = [...emojiList, newEmoji];
       setEmojiList(updatedEmojis);
     })
     .catch(error => alert(error.message))
   }
 
+  // if page elements are not yet loaded, display a loading message
   if(!isLoaded) {
     return <h3 className="loading-bar">Loading...</h3>
   }
-  
+
+  // else create routes to each page - /emoji/:name has an endpoint for each emoji card
   return (
     <BrowserRouter>
       <NavBar />
