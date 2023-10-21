67df001da04f072b6e6eb3b7698c4aaed53bb479
Connor <connordbrown@gmail.com>
2023-10-21 16:10:07 -0600

    comment EmojiCollection

--- src/components/EmojiCollection.js
+++ src/components/EmojiCollection.js
@@ -2,8 +2,11 @@ import React from "react";
 import EmojiCard from "./EmojiCard";
 import "./App.css";
 
+// creates collection of EmojiCard components and displays them as a group
+// param: emojis (Object array)
 function EmojiCollection({ emojis }) {
   const emojisToDisplay = emojis.map(emoji => {
+    // destructure emoji object to get specific properties
     const { id, htmlCode, name } = emoji;
     return <EmojiCard key={id}
                         id={id}
@@ -12,6 +15,7 @@ function EmojiCollection({ emojis }) {
     />
   })
 
+  // group emojisToDisplay
   return <div className="card-group">{emojisToDisplay}</div>
 }
 
